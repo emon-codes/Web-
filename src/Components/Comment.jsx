48f7e7a620e4d92168d12c6263e60b7409604346
emon ahmed <emon9996a@gmail.com>
2026-06-07 22:43:23 +0600

    some change

--- src/Components/Comment.jsx
+++ src/Components/Comment.jsx
@@ -1,19 +1,3 @@
-// import React from 'react'
-// import useComment from '../Hooks/useComment'
-
-// const Comment = () => {
-//     const { comment } = useComment();
-
-//   return (
-//       <div>
-//           {comment.map((coment) => (<h1>{ coment.id }</h1>) )}
-//     </div>
-//   )
-// }
-
-// export default Comment
-
-
 import React from 'react'
 import useComment from './../Hooks/useComment'
 
--- src/Hooks/useComment.js
+++ src/Hooks/useComment.js
@@ -1,20 +1,3 @@
-// import React, { useEffect, useState } from "react";
-
-// const useComment = () => {
-//   const [comment, setComment] = useState([]);
-//   useEffect(() => {
-//     fetch("comment.json")
-//       .then((res) => res.json())
-//       .then((data) => setComment(data));
-//   }, []);
-//   return {
-//     comment,
-//   };
-// };
-
-// export default useComment;
-
-
 import React, { useEffect, useState } from 'react'
 
 const useComment = () => {
