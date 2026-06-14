88bfe78e5b4190986488a1593d81cbb759c7e739
emon ahmed <emon9996a@gmail.com>
2026-06-14 21:19:58 +0600

    popularPoduct update

--- src/Pages/Home component/PopularProducts.jsx
+++ src/Pages/Home component/PopularProducts.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
-import SectionHeading from './SectionHeading'
-
+import React from "react";
+import SectionHeading from "./SectionHeading";
+import useData from "../../Hooks/useData";
 
 const PopularProducts = () => {
+  const {products} = useData()
   return (
     <div>
-  
-<SectionHeading heading={"Propular product"}  discription={"top rated product"}></SectionHeading>
+      <div className="flex justify-between items-center"  >
+        <div >
+          <SectionHeading
+            heading={"Propular product"}
+            discription={"top rated product"}
+          ></SectionHeading>
+        </div>
+        <div className="flex flex-row gap-6">
+          {products.map((product) => (<span>{ product.name }</span>) )}
 
-
-
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default PopularProducts
+export default PopularProducts;
