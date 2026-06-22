8fe315aac48ac0fa39158a45ea99379d29496e23
emon ahmed <emon9996a@gmail.com>
2026-06-22 20:03:01 +0600

    product detils update

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import useData from '../Hooks/useData'
+import { IoStarHalfOutline } from 'react-icons/io5'
 
 const Products = () => {
  const {products} = useData()
@@ -9,8 +10,29 @@ const Products = () => {
 
         <div >
 
-          <img className='w-70   border-4 border-green-500' src={product.image} alt="" />
+          <div><img className='w-70   border-4 border-green-500' src={product.image} alt="" /></div>
+          <div>
+            
+            <h5>{ product.category }</h5>
+            <h3>{product.name}</h3>
+            <h3> price {product.price}</h3>
 
+
+            <span className='flex gap-2 text-yellow-400'>
+              <IoStarHalfOutline />
+              <IoStarHalfOutline />
+              <IoStarHalfOutline />
+              <IoStarHalfOutline />
+              <IoStarHalfOutline />
+              
+            
+            
+            
+            </span>
+
+
+
+          </div>
 
 
         </div>
