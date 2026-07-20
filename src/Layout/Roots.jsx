88095e8d43db57fcb7b26d0efc82da3c3594e511
emon ahmed <emon9996a@gmail.com>
2026-07-20 21:33:12 +0600

    web site update

--- src/Layout/Roots.jsx
+++ src/Layout/Roots.jsx
@@ -1,21 +1,16 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import { Outlet } from 'react-router'
-import Products from '../Pages/Products'
-import Post from '../Components/Post'
-import Comment from '../Components/Comment'
-import Offer from '../Pages/Offer'
-import PopularProducts from '../Pages/Home component/PopularProducts'
+
 
 const Roots = () => {
   return (
       <div>
           <Navbar />
       <Outlet />
-      <Products />
-      <PopularProducts/>
-    <Offer/>
-          <div>footer</div>
+   
+
+       
 
 
     </div>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
+import Products from './Products'
+import PopularProducts from './Home component/PopularProducts'
+import Offer from './Offer'
 
 const Home = () => {
   return (
       <div>
-         
+           <Products />
+      <PopularProducts />
+          <Offer/>
     </div>
   )
 }
